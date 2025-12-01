46000d2e374893e7dcaa9c1b68d7cd79b4542dfb
Prottoy Roy <85828960+PragtR@users.noreply.github.com>
2025-12-01 14:18:11 -0500

    Update main.R

--- main.R
+++ main.R
@@ -1,6 +1,10 @@
 library(vroom) # faster for reading delimited data
 
-data_raw <- vroom("updated_histone_mods_and_GA_factors.feature_matrix.1kb_res.txt", 
+source (preprocess.R)
+
+data_raw <- vroom("/Users/prottoyroy/Desktop/php1560/updated_histone_mods_and_GA_factors.feature_matrix.1kb_res.txt", 
             delim = "\t", quote = "")
 
 data_clean <- preprocess_data(data_raw)
+
+
