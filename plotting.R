461609921f2cdf72a07f82db5e4237d0ed2b2ebe
Prottoy Roy <85828960+PragtR@users.noreply.github.com>
2025-12-07 22:29:05 -0500

    Update plotting.R

--- plotting.R
+++ plotting.R
@@ -39,13 +39,13 @@ plot_curves <- function(evaluation) {
 
 #' Plotting metrics for cross model
 #' 
-#' @description plots histograms for a given metric for the cross evaluated model
+#' @description plots barplots for a given metric for the cross evaluated model
 #' 
 #' @param cross_model_metrics A df containing the metrics of the cross trained model. The output of cross_chr_experiment
 #' @param metric A string containing the name of the metric to plot
 #' @param title A string for the title of the plot
 #' 
-#' @return nothing to return, but plots histograms of the metric for each test chromosome
+#' @return nothing to return, but plots barplots of the metric for each test chromosome
 plot_metric_cross_model <- function(cross_model_metrics, metric, title) {
   metrics <- cross_model_metrics[[metric]]
   ymin <- max(min(metrics) - 0.1, 0)
